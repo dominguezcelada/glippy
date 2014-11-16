45ecae18ef858bf3d65bd525fe60c5bdb33a8d87
oscardomin <oscar.dominguez.celada@est.fib.upc.edu>
2014-11-16 13:59:24 +0100

    Added selected attribute for every item's price.

--- GlippyDomain/src/main/java/com/glippy/entity/Price.java
+++ GlippyDomain/src/main/java/com/glippy/entity/Price.java
@@ -11,32 +11,45 @@ public class Price {
 
     private double price;
 
+    private boolean selected;
+
 
     // Constructors
 
     public Price() {
         this.supermarket = "mercadona";
         this.price = 0.0;
+        this.selected = false; //By default anyone is selected
     }
 
     public Price(double price) {
         this.supermarket = "mercadona";
         this.price = price;
+        this.selected = false; //By default anyone is selected
     }
 
     public Price(String supermarket) {
         this.supermarket = supermarket;
         this.price = 0.0;
+        this.selected = false;
     }
 
     public Price(String supermarket, double price) {
         this.supermarket = supermarket;
         this.price = price;
+        this.selected = false;
     }
 
     public Price(double price, String supermarket) {
         this.supermarket = supermarket;
         this.price = price;
+        this.selected = false;
+    }
+
+    public Price(String supermarket, double price, boolean selected) {
+        this.supermarket = supermarket;
+        this.price = price;
+        this.selected = selected;
     }
 
     // Setters
@@ -49,6 +62,10 @@ public class Price {
         this.price = price;
     }
 
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
     // Getters
 
     public String getSupermarket() {
@@ -59,7 +76,12 @@ public class Price {
         return price;
     }
 
+    public boolean isSelected() {
+        return selected;
+    }
+
     // Equals
+
 
     @Override
     public boolean equals(Object o) {
@@ -69,7 +91,8 @@ public class Price {
         Price price1 = (Price) o;
 
         if (Double.compare(price1.price, price) != 0) return false;
-        if (!supermarket.equals(price1.supermarket)) return false;
+        if (selected != price1.selected) return false;
+        if (supermarket != null ? !supermarket.equals(price1.supermarket) : price1.supermarket != null) return false;
 
         return true;
     }
@@ -78,9 +101,10 @@ public class Price {
     public int hashCode() {
         int result;
         long temp;
-        result = supermarket.hashCode();
+        result = supermarket != null ? supermarket.hashCode() : 0;
         temp = Double.doubleToLongBits(price);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (selected ? 1 : 0);
         return result;
     }
 }
